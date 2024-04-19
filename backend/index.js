c96278643d70c74d81741a7da538e138bb4f3091
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-04-19 21:58:56 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./db");
-const authRoute = require("./router/authRoute");
+const authRoute = require("./routes/authRoute");
 const cors = require("cors");
 
 const app = express();
