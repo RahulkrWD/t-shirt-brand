9bbc357b0f6e6ab8505ca5f9339d466d42bf2c59
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-04-22 07:14:54 +0530

    update footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,24 +1,3 @@
-// import React from "react";
-
-// function Footer() {
-//   return (
-//     <div className="bg-dark text-light p-3">
-//       <div className="social media p-2">
-//         <i className="fa-brands fs-2 text-danger p-2 fa-instagram"></i>
-//         <i className="fa-brands text-primary  fs-2 p-2 fa-facebook-f"></i>
-//         <i className="fa-brands text-success fs-2 p-2 fa-whatsapp"></i>
-//       </div>
-//       <h6>+91 7320810579</h6>
-//       <h6>rahulkrnodejs@gmail.com</h6>
-//       <h6 className="m-1">
-//         <i className="fa-regular fa-copyright"></i> Copyrights 2024. Nova All
-//         Rights Reserved.
-//       </h6>
-//     </div>
-//   );
-// }
-
-// export default Footer;
 import React from "react";
 
 function Footer() {
