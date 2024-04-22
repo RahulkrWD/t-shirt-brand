0b5fe7161555278839d367417286a6a971654aaf
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-04-22 09:52:55 +0530

    modify navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,144 +1,137 @@
-import React, { useState } from "react";
-import styles from "./styles/Navbar.module.css";
-import CartIcons from "./CartIcons";
-import { Link, useNavigate } from "react-router-dom";
-import Select from "react-select";
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-function Navbar() {
-  const [paused, setPaused] = useState(false);
-  const navigate = useNavigate();
-  const user = localStorage.getItem("auth");
-  let userName;
-  if (user) {
-    try {
-      const parsedUser = JSON.parse(user);
-      const splitName = parsedUser.user.name.split(" ")[0];
-      userName = splitName.charAt(0).toUpperCase() + splitName.slice(1);
-    } catch (error) {
-      console.log("split error");
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: 200,
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    width: "100%",
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={Boolean(anchorEl)}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Login</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Register</MenuItem>
+    </Menu>
+  );
+  const getAbbreviatedTitle = () => {
+    const title = "Nova";
+    const maxWidth = 400;
+    if (window.innerWidth < maxWidth) {
+      return title.charAt(0);
     }
-  }
-
-  function handleLogout() {
-    localStorage.removeItem("auth");
-    navigate("/login");
-  }
-
-  function mouseEnter() {
-    setPaused(true);
-  }
-
-  function mouseLeave() {
-    setPaused(false);
-  }
+    return title;
+  };
 
   return (
-    <>
-      <div className={styles.nav_bg}>
-        <div
-          className={`${styles.nav_icons} ${paused ? styles.paused : ""}`}
-          onMouseEnter={mouseEnter}
-          onMouseLeave={mouseLeave}
-        >
-          <span className={`text-light ${styles.nav_contact}`}>
-            <i className="fa-solid m-1 text-light text-primary fa-phone-volume"></i>
-            +91 999-99-99-99
-          </span>
-
-          <span className={`text-light ${styles.nav_mail}`}>
-            <i className="fa-regular m-1 text-white fa-envelope"></i>
-            novabrand99@gmail.com
-          </span>
-
-          <span className={`text-light ${styles.nav_offer}`}>
-            🌟 Limited Time Offer: Get 5% Off Orders Over ₹499! Use Code:
-            SAVE5NOW 🌟
-          </span>
-        </div>
-      </div>
-      <div className={`navbar ${styles.nav_cont}`}>
-        <div className="title_brand">
-          <Link to={"/homepage"} className="text-decoration-none">
-            <strong className={styles.brand_name}>
-              No<strong className="text-primary">va</strong>
-            </strong>
-          </Link>
-        </div>
-        <div className={styles.search_div}>
-          <Select
-            className={styles.input_search}
-            // options={filteredOptions}
-            // onInputChange={handleInputChange}
-            // onChange={handleSelectChange}
-            isClearable={true}
-            placeholder="Search here..."
-          />
-        </div>
-        <div className={styles.list}>
-          <ul className={styles.list_items}>
-            {user ? (
-              <li className="dropdown">
-                <Link
-                  className="text-decoration-none fw-bold"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <strong className="text-danger">Hi, </strong> {userName}
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="/Profile">
-                      Profile
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="/My-order">
-                      My Order
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="dropdown-item"
-                      onClick={handleLogout}
-                      to="/login"
-                    >
-                      Log Out
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-            ) : (
-              <li className="fw-bold">
-                <Link className="text-decoration-none" to="/login">
-                  Login
-                </Link>
-                /
-                <Link className="text-decoration-none" to="/register">
-                  Register
-                </Link>
-              </li>
-            )}
-
-            <li>
-              <Link to={"/cart"}>
-                <CartIcons />
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.responsive_search}>
-          <Select
-            className={styles.responsive_search_input}
-            // options={filteredOptions}
-            // onInputChange={handleInputChange}
-            // onChange={handleSelectChange}
-            isClearable={true}
-            placeholder="Search here..."
-          />
-        </div>
-      </div>
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            {getAbbreviatedTitle()}
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <IconButton size="large" aria-label="show cart" color="inherit">
+            <ShoppingCartIcon />
+          </IconButton>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Button color="inherit" onClick={handleProfileMenuOpen}>
+              Login
+            </Button>
+            <Button color="inherit" onClick={handleProfileMenuOpen}>
+              Register
+            </Button>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </Box>
   );
 }
-
-export default Navbar;
