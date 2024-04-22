a485d21217b9b0f898f1da8102c48b221b80aa43
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-04-22 07:13:49 +0530

    update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,25 +1,44 @@
+// import React from "react";
+
+// function Footer() {
+//   return (
+//     <div className="bg-dark text-light p-3">
+//       <div className="social media p-2">
+//         <i className="fa-brands fs-2 text-danger p-2 fa-instagram"></i>
+//         <i className="fa-brands text-primary  fs-2 p-2 fa-facebook-f"></i>
+//         <i className="fa-brands text-success fs-2 p-2 fa-whatsapp"></i>
+//       </div>
+//       <h6>+91 7320810579</h6>
+//       <h6>rahulkrnodejs@gmail.com</h6>
+//       <h6 className="m-1">
+//         <i className="fa-regular fa-copyright"></i> Copyrights 2024. Nova All
+//         Rights Reserved.
+//       </h6>
+//     </div>
+//   );
+// }
+
+// export default Footer;
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div
-      className="bg-dark text-light p-5 text-center"
-      style={{ height: "300px" }}
-    >
-      <h3 className="text-center m-5">
-        All Right Reserved &copy; Developer 2024.
-      </h3>
-      <p className="text-center m-3 footer">
-        <a className="m-1 text-decoration-none text-light" href="#about">
-          About
-        </a>
-        |
-        <a className="m-1 text-decoration-none text-light" href="#contact">
-          Contact
-        </a>
-        |
-      </p>
+    <div className="bg-dark text-light p-3">
+      <h6>+91 7320810579</h6>
+      <h6>rahulkrnodejs@gmail.com</h6>
+      <h6 className="m-1 footer_reserved">
+        <i className="far fa-copyright"></i> Copyrights 2024. Nova All Rights
+        Reserved.
+      </h6>
+      <style>
+        {`
+          @media (max-width: 450px) {
+            .footer_reserved {
+              font-size: 14px;
+            }
+          }
+        `}
+      </style>
     </div>
   );
 }
